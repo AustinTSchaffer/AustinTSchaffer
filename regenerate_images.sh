796ea745ecdbc019d6159742bd647925466194c3
Austin Schaffer <schaffer.austin.t@gmail.com>
2021-02-06 16:35:17 -0500

    Adjusted overview image

--- regenerate_images.sh
+++ regenerate_images.sh
@@ -2,6 +2,6 @@
 
 common_opts="username=AustinTSchaffer&count_private=true&show_icons=true&theme=gruvbox"
 
-wget "https://github-readme-stats.vercel.app/api?$common_opts&hide=stars" -O ./images/overview.svg
+wget "https://github-readme-stats.vercel.app/api?$common_opts&hide_rank=true" -O ./images/overview.svg
 
 wget "https://github-readme-stats.vercel.app/api/top-langs/?$common_opts&langs_count=10&layout=compact&hide=html,css,scss,less" -O ./images/lang-stats.svg
